1aae90413d7b2b91da13187e28bb037819086a79
PROVAKAR DATTA <provakardatta@iMac.local>
2022-05-27 15:34:42 -0400

    header file with fundamental constant added.

---
+++ include/Constants.h
@@ -0,0 +1,22 @@
+#ifndef CONST_H
+#define CONST_H
+
+namespace constant {
+
+  // electron
+  static const double Me = 0.5109989461E-03; // +/- 31E-13 GeV
+
+  // muon
+  static const double Mmu = 0.1056583745; // +/- 24E-10 GeV
+  
+  // proton
+  static const double Mp = 0.938272081; // +/- 6E-9 GeV
+  static const double mup = 2.7928473446; // +/- 7E-11 muN
+
+  // neutron
+  static const double Mn = 0.939565413; // +/- 6E-9 GeV
+  static const double mun = -1.9130427; // +/- 5E-7 muN
+
+}
+
+#endif
