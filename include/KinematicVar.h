7d4f26b3575f46e71a58366494164c5692aea2bb
PROVAKAR DATTA <provakardatta@iMac.local>
2022-05-27 15:34:53 -0400

    Header file to calculate common kinematic variables added.

---
+++ include/KinematicVar.h
@@ -0,0 +1,14 @@
+#ifndef KINE_VAR_H
+#define KINE_VAR_H
+
+#include "../include/Constants.h"
+
+namespace kine{
+
+  double Q2(double ebeam, double eeprime, double etheta); // GeV, GeV, rad
+  double W2(double ebeam, double eeprime, double Q2); // GeV, GeV, GeV2
+  double W(double ebeam, double eeprime, double Q2); // GeV, GeV, GeV2
+  
+}
+
+#endif
