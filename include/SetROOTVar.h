d7498ebafb318054d26ce4869155839fb47015ac
Provakar Datta <pdbforce@ifarm1801.jlab.org>
2022-05-29 10:36:53 -0400

    Added constructors to handle Ndata objects.

--- include/SetROOTVar.h
+++ include/SetROOTVar.h
@@ -3,20 +3,26 @@
 
 #include "TTree.h"
 
-namespace setvar {
+namespace setrootvar {
 
-  // PS cluster
-  /* void setpsclusvar(); */
-  /* void setpsclustimevar(TTree *T, double ps_atimeblk); */
-
-  /* void setshclvar(); */
-
-  /* void sethcalclvar(); */
-
-  void setbranch(TTree *T,
+  void setbranch(TTree *T, //use to settreebranch for one variable
+		 std::string prefix,
+		 std::string suffix,
+		 void* memory);
+  void setbranch(TTree *T, //use to settreebranch for multiple variables
 		 std::string prefix,
 		 std::vector<std::string> suffix,
-		 std::vector<double*> memory);
+		 std::vector<void*> memory);
+  void setbranch(TTree *T, //use if one of the variables is Ndata
+		 std::string prefix,
+		 std::vector<std::string> suffix,
+		 std::vector<void*> memory,
+		 int ndatavarpos);
+  void setbranch(TTree *T, //use if there are multiple Ndata variables
+		 std::string prefix,
+		 std::vector<std::string> suffix,
+		 std::vector<void*> memory,
+		 std::vector<int> ndatapos);
 
 }
 
--- src/SetROOTVar.cpp
+++ src/SetROOTVar.cpp
@@ -1,18 +1,22 @@
 #include "../include/SetROOTVar.h"
 
-namespace setvar {
-
-  // PS Cluster
-
-  // void setpsclustimevar( TTree *T, double &ps_atimeblk){
-  //   T->SetBranchStatus("bb.ps.atimeblk",1);
-  //   T->SetBranchAddress("bb.ps.atimeblk",ps_atimeblk);
-  // }
+namespace setrootvar {
 
   void setbranch(TTree *T,
 		 std::string prefix,
+		 std::string suffix,
+		 void* memory)
+  {
+    std::string branchname;
+    branchname = prefix + std::string(".") + suffix;
+    T->SetBranchStatus(branchname.c_str(),1);
+    T->SetBranchAddress(branchname.c_str(),memory);
+  }
+  //--------------------------------------------
+  void setbranch(TTree *T,
+		 std::string prefix,
 		 std::vector<std::string> suffix,
-		 std::vector<double*> memory)
+		 std::vector<void*> memory)
   {
     std::string branchname;
     for(int i=0;i<suffix.size();i++){
@@ -21,5 +25,45 @@ namespace setvar {
       T->SetBranchAddress(branchname.c_str(),memory[i]);
     }
   }
-
+  //--------------------------------------------
+  void setbranch(TTree *T,
+  		 std::string prefix,
+  		 std::vector<std::string> suffix,
+  		 std::vector<void*> memory,
+  		 int ndatavarpos)
+  {
+    std::string branchname;
+    for(int i=0;i<suffix.size();i++){
+      if(i!=ndatavarpos){
+  	branchname = prefix + std::string(".") + suffix[i];
+  	T->SetBranchStatus(branchname.c_str(),1);
+  	T->SetBranchAddress(branchname.c_str(),memory[i]);
+      }else{
+  	branchname = std::string("Ndata.")+prefix+std::string(".")+suffix[i];
+  	T->SetBranchStatus(branchname.c_str(),1);
+  	T->SetBranchAddress(branchname.c_str(),memory[i]);
+      }
+    }
+  }
+  //--------------------------------------------
+  void setbranch(TTree *T,
+  		 std::string prefix,
+  		 std::vector<std::string> suffix,
+  		 std::vector<void*> memory,
+  		 std::vector<int> ndatapos)
+  {
+    std::string branchname;
+    for(int i=0;i<suffix.size();i++){
+      if(std::find(ndatapos.begin(),ndatapos.end(),i)!=ndatapos.end()){
+  	branchname = std::string("Ndata.")+prefix+std::string(".")+suffix[i];
+  	T->SetBranchStatus(branchname.c_str(),1);
+  	T->SetBranchAddress(branchname.c_str(),memory[i]);
+      }else{
+ 	branchname = prefix + std::string(".") + suffix[i];
+  	T->SetBranchStatus(branchname.c_str(),1);
+  	T->SetBranchAddress(branchname.c_str(),memory[i]);
+      }
+    }
+  }
+  //--------------------------------------------
 }
