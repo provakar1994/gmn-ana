89b51bb1b6c8bc8a97d938800fa6c645a8eddd8d
Provakar Datta <pdbforce@ifarm1901.jlab.org>
2022-08-17 11:30:42 -0400

    shell script to vary R to get chi2 over a range of choice

---
+++ scripts/pdatta/simulation/varyR.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+imin=$1
+imax=$2
+
+for (( i=$imin; i<$imax; i++ ))
+do
+    # We need to do some floating point arithmetic, 
+    # let's use bc (basic/bench calculator)
+    R=$(echo "scale=2; (80+$i)/100" | bc)
+    analyzer -b -q 'simu_data_fit.cpp("sbs4_sbs50p_hcaldxdy_simu.cfg",'$R',"simu_data_fit_SBS50_.82T_R'$R'.root")'
+done
+
+# R=$1
+# analyzer -b -q 'simu_data_fit.cpp("sbs4_sbs50p_hcaldxdy_simu.cfg",'$R',"simu_data_fit_SBS50_.82T_R'$R'.root")'
+
