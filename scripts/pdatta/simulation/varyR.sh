16b831ec1b174c9cd56e532bbe158668c1ee4a51
Provakar Datta <pdbforce@ifarm1901.jlab.org>
2022-08-17 12:46:45 -0400

    minor change

--- scripts/pdatta/simulation/varyR.sh
+++ scripts/pdatta/simulation/varyR.sh
@@ -8,7 +8,7 @@ do
     # We need to do some floating point arithmetic, 
     # let's use bc (basic/bench calculator)
     R=$(echo "scale=2; (80+$i)/100" | bc)
-    analyzer -b -q 'simu_data_fit.cpp("sbs4_sbs50p_hcaldxdy_simu.cfg",'$R',"simu_data_fit_SBS50_.82T_R'$R'.root")'
+    analyzer -b -q 'simu_data_fit.cpp("sbs4_sbs50p_hcaldxdy_simu.cfg",'$R',"siout/simu_data_fit_SBS50_.82T_R'$R'.root")'
 done
 
 # R=$1
