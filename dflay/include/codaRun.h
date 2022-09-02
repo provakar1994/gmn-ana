284e1b261aea169399aaa235bff3d78bb729659e
Provakar Datta <pdbforce@ifarm1802.jlab.org>
2022-09-02 13:16:38 -0400

    Added functionality to store every beg-end segment number pair for a particular run. This is needed for sorting the root files before parsing.

--- dflay/include/codaRun.h
+++ dflay/include/codaRun.h
@@ -1,20 +1,30 @@
 #ifndef UTIL_CODA_RUN_H
 #define UTIL_CODA_RUN_H
 
+#include <vector>
+
 // a simple struct for CODA run info 
 
 typedef struct codaRun {
-   std::string info;       // miscellaneous user info 
-   std::string rfPrefix;   // directory where ROOT files live 
-   int stream;             // EVIO file stream
-   int segmentBegin;       // EVIO beginning segment 
-   int segmentEnd;         // EVIO end segment 
-   int numFiles;           // number of ROOT files
-   int runNumber;          // run number 
-   // constructor 
-   codaRun(): 
-      info("NONE"),stream(0),segmentBegin(0),segmentEnd(0),numFiles(0),runNumber(0)
-   {}
+  std::string info;       // miscellaneous user info 
+  std::string rfPrefix;   // directory where ROOT files live 
+  int stream;             // EVIO file stream
+  /* std::vector<int> segmentBegin;       // EVIO beginning segment  */
+  /* std::vector<int> segmentEnd;         // EVIO end segment */ 
+  std::vector<pair<int, int>> segmentBeg_End; // EVIO beginning & Ending segment in individual root files for a given run.
+  int numFiles;           // number of ROOT files
+  int runNumber;          // run number 
+
+  // constructor 
+codaRun(): 
+  info("NONE"),stream(0),segmentBeg_End({}),numFiles(0),runNumber(0)
+  {}
+
+  void Clear() 
+  {
+    segmentBeg_End.clear();
+  }
+
 } codaRun_t; 
 
 #endif 
