42d805dad2b4edfe27424255c5a86b02a5e025d9
Provakar Datta <pdbforce@ifarm1802.jlab.org>
2022-10-05 18:41:10 -0400

    Added HCAL block dimension info

--- include/ExpConstants.h
+++ include/ExpConstants.h
@@ -7,12 +7,20 @@
 namespace expconst {
 
   // Detector variables
+  // bbcal
   static const int shcol = 7;
   static const int shrow = 27;
   static const int pscol = 2;
   static const int psrow = 26;
+  // hcal [dimesions found from g4sbs: G4SBSHArmBuilder::MakeHCALV2]
   static const int hcalcol = 12;
   static const int hcalrow = 24;
+  static const double hcalblk_w = 0.1524;       //m, width of a HCAL block
+  static const double hcalblk_h = 0.1524;       //m, height of a HCAL block
+  static const double hcalblk_cTc_h = 0.15494;  //m, horizontal center-to-center dist.
+  static const double hcalblk_cTc_v = 0.15875;  //m, vertical center-to-center dist.
+  static const double hcalblk_gap_h = 0.00254;  //m, horiz. gap bet. two blocks
+  static const double hcalblk_gap_v = 0.00635;  //m, vert. gap bet. two blocks
   
   // Constant for the entire experiment
   // target
